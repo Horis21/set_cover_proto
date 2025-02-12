083c946977dce1e7a6d4079b3842c232ee8f9bab
Horis21 <Radu-1@student.tudelft.nl>
2025-02-12 19:16:25 +0200

    no pruning results

--- classes/Solver.py
+++ classes/Solver.py
@@ -465,7 +465,7 @@ class Solver:
             self.add_back_to_pq(node) # Add back to PQ to reupdate order
 
         size, depth = root.print_solution()
-        self.cache.write_bounds(self.name)
+        # self.cache.write_bounds(self.name)
         # start_time = time.time()
         # root.queryAll(orig_df)
         # elapsed_time = time.time() - start_time
